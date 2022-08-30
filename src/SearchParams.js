80031e78d53903306d7e7fa0dc8dbd49983e0bb3
YousfiHamza <yousfihamza22@gmail.com>
2022-08-30 07:44:15 +0100

    fix: error free console ( how satisfying lol )

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
+// const location = "Seattle, WA";
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const handleChange = (e) => {
+    setLocation(e.target.value);
+  };
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={handleChange}
+          />
         </label>
         <button>Submit</button>
       </form>
